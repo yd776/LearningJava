1fb07c4d350e2f3144240f40beba693fa2e38461
yd2506 <76481952+yd776@users.noreply.github.com>
2023-10-01 10:49:51 +0530

    Create week2.java

---
+++ week2.java
@@ -0,0 +1,26 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        float f=22/7;
+        System.out.println(f);
+        System.out.println(Math.pow(2,3));
+        int a=0;
+        int b=10;
+        a++;
+        b--;
+        System.out.println(a);
+        System.out.println(b);
+        String s="hello";
+        String t="world";
+        System.out.println(s +" "+t);
+        s=s.substring(0,3)+"p!";
+        System.out.println(s);
+        System.out.println(s.length());
+        long b1=22/7;
+        System.out.println(b1);
+        int[] arr = {2,3,4};
+        System.out.println(arr);
+
+        
+    }
+}
