b1cb04fb0c0cee70ec7e7f5d244ca2208a329dd5
yd2506 <76481952+yd776@users.noreply.github.com>
2023-10-01 22:40:48 +0530

    Create week3.java

---
+++ week3.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int a=5;
+        int b=4;
+        if (b<a){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(1);
+        }
+    }
+}
