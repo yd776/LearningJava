615abdb584348cc677de6e3737799caf6434a5c1
yd2506 <76481952+yd776@users.noreply.github.com>
2023-11-18 13:40:09 +0530

    Update variablesandliterals.java

--- variablesandliterals.java
+++ variablesandliterals.java
@@ -32,4 +32,12 @@
             System.out.println(result);
             System.out.println(result1);
 
+         (String[] args){
+        int x=7;
+        int y=5;
+        int a=5;
+        int b=9;
+        boolean result=x>y && a<b;
+        System.out.println(result);
+
 
