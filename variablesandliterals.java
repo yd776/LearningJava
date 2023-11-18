a679ea99d78582e596d8a77fbabbfa0009b15114
yd2506 <76481952+yd776@users.noreply.github.com>
2023-11-18 12:47:00 +0530

    Create variablesandliterals.java

---
+++ variablesandliterals.java
@@ -0,0 +1,15 @@
+        System.out.println("hellow world");
+        int num1=7;
+        int num2=8;
+        float f1=5.0f;
+        double d1=5.02;
+        double result;
+        result=d1+f1;
+        System.out.println(result);
+        // literals 
+        // We can perform many action on the variables that we are putting in 
+        char c='a';
+        c++;
+        System.out.println(c);//should give b
+        //casting conversion of one frmat to another
+        
