38c22327fbe59963fb7f0071e5eb9f6b3d348b77
yd2506 <76481952+yd776@users.noreply.github.com>
2023-12-19 19:32:14 +0530

    Create leetcode1.java

---
+++ leetcode1.java
@@ -0,0 +1,16 @@
+class Main{
+  public static void main(String[] args){
+    int[] nums={3,2,4};
+    int target=6;
+    for(int i=0;i<nums.length;i++){
+      for(int j=i+1;j<nums.length;j++){
+        if(nums[i]+nums[j]==target){
+          System.out.println(i+" "+j);
+        }
+          
+        }
+      }
+      
+    }
+    
+  }
