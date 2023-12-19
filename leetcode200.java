284cb5e085c9e6c81d5a171e9abc6324bb6d7311
yd2506 <76481952+yd776@users.noreply.github.com>
2023-12-19 09:14:52 +0530

    Create leetcode200.java

---
+++ leetcode200.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if  (grid[i][j]=='1'){
+                    count+=1;
+                    callbfs(grid,i,j);
+
+                }
+                
+
+            }
+        }
+
+        return count;
+    }
+    public void callbfs(char[][] grid,int i,int j){
+        if(i<0||i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]=='0'){
+            return;
+        }
+            grid[i][j]='0';
+            callbfs(grid,i+1,j);
+            callbfs(grid,i-1,j);
+            callbfs(grid,i,j+1);
+            callbfs(grid,i,j-1);
+        
+        
+    }
+}
