de65b418d759771ddd9b44cc9ccb474f3e67e581
yd2506 <76481952+yd776@users.noreply.github.com>
2024-02-20 10:41:26 +0530

    Create main.java

---
+++ overriding/main.java
@@ -0,0 +1,10 @@
+class main{
+    public static void main(String args[]){
+        sbi s= new sbi();
+        icici i=new icici();
+        axis a=new axis();
+        System.out.println("SBI RATE OF INTERST IS " + s.getRateofInterest());
+        System.out.println("ICICI Rate of interest is "+ i.getRateofInterest());
+        System.out.println("Axis rate of interest is "+a.getRateofInterest());
+    }
+}
