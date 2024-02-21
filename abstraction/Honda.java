27efe0a251c34bf1e99d8d45bc19d399e5ee68a0
yd2506 <76481952+yd776@users.noreply.github.com>
2024-02-21 10:05:13 +0530

    Create Honda.java

---
+++ abstraction/Honda.java
@@ -0,0 +1,9 @@
+class Honda extends Bike{
+    public void run(){
+        System.out.println("Running propperly");
+    }
+    public static void main(String args[]){
+    Bike b1 =new Honda();
+    b1.run();
+}
+}
