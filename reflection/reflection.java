b6614c814a68bddd13067d811c8fd0361eca7019
yd2506 <76481952+yd776@users.noreply.github.com>
2024-03-03 12:06:19 +0530

    Update reflection.java

--- reflection/reflection.java
+++ reflection/reflection.java
@@ -1,7 +1,39 @@
+/******************************************************************************
 
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 public class reflection
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		cat c =new cat("snowwhite",2);
+		Field [] catfields=c.getClass().getDeclaredFields();
 		
+		for(Field field:catfields){
+		    if(field.getName().equals("name")){
+		        field.setAccessible(true);
+		        field.set(c,"james mgill");
+		    }
+		}
+		
+		System.out.println(c.getname());
+		
+		Method[] catmethods=c.getClass().getDeclaredMethods();
+		
+		for(Method method :catmethods){
+		    
+		    if(method.getName().equals("thisisapublicmethod")){
+		        method.setAccessible(true);
+		        method.invoke(null);
+		    }
+		    
+		}
+	
 	}
 }
