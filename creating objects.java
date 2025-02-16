8c88bb60154eb294ff4f92eff5e7723c01dc7617
yd2506 <76481952+yd776@users.noreply.github.com>
2025-02-16 19:24:29 +0530

    Update creating objects.java

--- creating objects.java
+++ creating objects.java
@@ -80,3 +80,73 @@ public class FClass
 		        e1.displayDetails();
 	    }
 }
+
+
+
+
+
+
+
+
+
+
+
+	import java.util.*;
+
+class Employee
+{
+    String eid;
+    String ename;
+    String eprojects[];
+ //Define all the required methods here
+ 
+ public void Employee(String id,String name, String[] projects){
+     
+     eid=id;
+     ename=name;
+     eprojects=projects;
+ }
+ public void Employee(Employee e2){
+     
+     eid=e2.eid;
+     ename=e2.ename;
+     eprojects=e2.eprojects;
+ }
+ public void Display(){
+     System.out.println("id:"+eid);
+     System.out.println("name:"+ename);
+     System.out.println("projects:");
+     for (String s: eprojects){
+         System.out.print(s +":");
+     }
+     Sytem.out.println("\n");
+ }
+ 
+ 
+public void mutator()
+    {
+        this.ename = "Mr "+ this.ename;
+        this.eprojects[0] = null;
+    }
+    
+}
+public class FClass
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+    	   String project[] = {"P001","P002","P003"};
+        //Enter the id of employee
+        String id = s.nextLine();
+        //Enter the name of employee
+        String name = s.nextLine();
+        
+        Employee e1 = new Employee(id,name,project);
+        Employee e2 = new Employee(e1); 
+        //The copy constructor must copy all the data members. 
+       
+        e1.mutator();
+        
+        e2.display();
+    }
+}
