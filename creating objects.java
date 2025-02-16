3de5743462d34befb1863c78d9a261284033d675
yd2506 <76481952+yd776@users.noreply.github.com>
2025-02-16 12:47:07 +0530

    Update creating objects.java

--- creating objects.java
+++ creating objects.java
@@ -1,9 +1,9 @@
-class Calculator{
+class Calculator{ //creating a new class named calculator
     
     int a;
-    public int add(int n1,int n2)
+    public int add(int n1,int n2)// creating a method so that i can call the functions directly
     {
-        System.out.println("adding the two numbers");
+        System.out.println("adding the two numbers"); 
         int r= n1+n2;
         return r;
     }
@@ -13,7 +13,7 @@ class Main {
     public static void main(String[] args) {
         int num1=4;
         int num2=5;
-        Calculator calc =new Calculator();
+        Calculator calc =new Calculator(); // creating a reference variable so that i can call the functions in calculator class
         int result= calc.add(num1,num2);
         
         //int result = num1+num2;
