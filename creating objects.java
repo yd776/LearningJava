c12b164f32ccd0c1db0192401224a628a6d60d82
yd2506 <76481952+yd776@users.noreply.github.com>
2025-02-16 19:10:27 +0530

    Update creating objects.java

--- creating objects.java
+++ creating objects.java
@@ -20,3 +20,63 @@ class Main {
         System.out.println(result);
     }
 }
+
+
+// Week2 GA2.2 create methods and constructors for a questions
+
+
+
+import java.util.Scanner;
+
+class Employee{
+	    String ename;
+	    String eid;
+	    String edept;
+	    
+	    public Employee(){
+		        ename = "guest";
+	    }
+//Define the required methods
+
+        public void Emplaoyee(name,id,dept){
+            ename=name;
+            eid=id;
+            edept=dept;
+            
+        }
+        public void copyDept(Employee e2){
+            
+            this.edept=e2.edept;
+        }
+        public void displayDetails(){
+            
+            System.out.println("ename : "+ename);
+            System.out.println("eid : "+eid);
+            System.out.println("edept : "+edept);
+        }
+
+     
+public class FClass 
+{
+	    public static void main(String args[]) 
+	    {
+		        Scanner s = new Scanner(System.in);
+		        Employee e1 = new Employee();
+		
+		        //Enter name of the employee
+		        String name = s.nextLine();
+		
+		        //Enter id of the employee
+		        String id = s.nextLine();
+		
+		        //Enter department of the employee
+		        String dept = s.nextLine();
+		
+		        Employee e2 = new Employee(name,id,dept);
+		
+		        e1.copyDept(e2); 
+		        //Copies the department name of e2 into e1's department name.
+		
+		        e1.displayDetails();
+	    }
+}
