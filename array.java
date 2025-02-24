c25473a04716994de5bb2094328c97967cd31351
yd2506 <76481952+yd776@users.noreply.github.com>
2025-02-24 16:32:06 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int arr1[] = new int[4];
+        arr1[0]=1;
+        arr1[2]=2;
+        arr1[3]=3;
+        
+        for(int i=0; i<=arr1.length;i++){
+            
+            System.out.println(arr1[i]);
+        }
+        }
+}
